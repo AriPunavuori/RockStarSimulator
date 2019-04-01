f60633f2eeb53666a7c9c60d1e714dfc228410b7
HTC Vive workstation <MrNobody@NoWhere.com>
2019-04-01 16:43:51 +0300

    Drawer works and stuff move inside 2!

---
+++ RockStarSimulator/Assets/Scripts/BoxContentMover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxContentMover : MonoBehaviour
+{
+    Vector3 lastpos;
+    public Vector3 Inside;
+
+    private void Awake() {
+        lastpos = transform.position;
+    }
+
+    private void FixedUpdate() {
+        if (lastpos != transform.position) {
+            var movementAmount = transform.position - lastpos;
+            var colliders = Physics.OverlapBox(transform.position, Inside * .5f, transform.rotation);
+            foreach (var c in colliders) {
+                print(c);
+                var rb = c.GetComponent<Rigidbody>();
+                if(rb != null)
+                rb.position += movementAmount;
+            }
+        }
+        lastpos = transform.position;
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(Vector3.zero, Inside);
+    }
+}
