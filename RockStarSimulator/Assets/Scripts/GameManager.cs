7998d5bd7d6da5720dee8e14a7fc10d0a8ff90fb
HTC Vive workstation <MrNobody@NoWhere.com>
2019-04-02 13:39:33 +0300

    OnJointBreak calls TrashObject, TextMeshPro, GameManager, TotalTrashedCounter

---
+++ RockStarSimulator/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour {
+    public TextMeshProUGUI uiText;
+    float totalCost = 0;
+    public float textTimer;
+    public float textTime = 5;
+
+    void Start() {
+        textTimer = textTime;
+        UpdateUI("" + totalCost);
+    }
+
+
+    private void Update() {
+        if (textTimer < 0)
+            UpdateUI("");
+        textTimer -= Time.deltaTime;
+    }
+
+
+    void UpdateUI(string s) {
+        if (s != "")
+            uiText.text = "So far you have trashed " + s + "$ worht of shit!";
+        else
+            uiText.text = "";
+        textTimer = textTime;
+    }
+
+    public void ObjectTrashed(float v) {
+        totalCost += v;
+        UpdateUI("" + totalCost);
+    }
+
+}
