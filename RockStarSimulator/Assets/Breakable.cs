21a544570a25e8a88bcc529643d85089eeeb0089
OPISTO\leo.virolainen <virolainen.leo@gmail.com>
2019-04-03 12:54:14 +0300

    bottle

---
+++ RockStarSimulator/Assets/Breakable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour {
+
+    public GameObject okVer;
+    public GameObject destroyedVer;
+    CapsuleCollider cc;
+    Object
+    public float health = 50f;
+
+
+    private void Start() {
+        cc = GetComponent<CapsuleCollider>();
+    }
+
+    public void TakeDamage(float amount) {
+        health -= amount;
+        if (health <= 0f) {
+            Break();
+        }
+    }
+
+    void Break() {
+        if (destroyedVer != null) {
+            destroyedVer.gameObject.SetActive(true);
+            okVer.gameObject.SetActive(false);
+            cc.enabled = false;
+            Destroy(gameObject, 8);
+            //Instantiate(destroyedVer, transform.position, transform.rotation);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+}
